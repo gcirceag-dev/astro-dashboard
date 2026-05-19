012216de70db454f5c3692f4fadc8a903bf7e542
gcirceag-dev <gcirceag@gmail.com>
2026-05-19 08:24:04 +0300

    Add files via upload

--- da.py
+++ da.py
@@ -1239,7 +1239,7 @@ with tab1:
             font-size: 22px;
         }
         .tabel-soare td {
-            padding: 12px 16px;
+            padding: 2px 16px;
             border-bottom: 1px solid #e0e0e0;
         }
         .tabel-soare td:first-child {
